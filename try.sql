555098ef214bb562a44e724288de7eb761e21ec8
leonardocunha13 <leonardo.dejesus@heroengineering.com.au>
2025-03-27 12:10:18 +0800

    Correct

--- try.sql
+++
@@ -1,7 +0,0 @@
-insert into login (username, password) values ('admin', 'admin');
-insert into login (username, password) values ('user', 'user');
-insert into login (username, password) values ('guest', 'guest');
-
-UPDATE Login
-SET Name = 'Administ', Role= 'Head'
-WHERE Username = 'Admin';
